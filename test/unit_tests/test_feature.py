c02a8f8d040442f08f06531141bee8506cd17223
amirali-ashraf <57413225+amiraliash@users.noreply.github.com>
2022-02-14 17:50:09 -0500

    feature is tested, needs numpy redo

---
+++ test/unit_tests/test_feature.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+from genrecog.preprocess.feature import Feature
+
+
+
+def test_torch_fbank_features():
+    feature_maker = Feature(sample_rate=16_000, n_fft=400)
+    tensor = torch.rand([10, 16_000])
+    features = feature_maker.torch_fbank_features(tensor)
+    assert features.shape == (10, 101, 40)
+
+def test_numpy_fbank_features():
+    feature_maker = Feature()
+    np_array = np.random.randn(10, 16_000)
+    features = feature_maker.numpy_fbank_features(np_array)
+    assert features.shape == (10, 101, 40)
