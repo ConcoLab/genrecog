1331ee72e62bdb2f4c61c5466cde9e702459faa5
amirali-ashraf <57413225+amiraliash@users.noreply.github.com>
2022-02-14 17:49:51 -0500

    processor is added

---
+++ genrecog/preprocess/preprocessor.py
@@ -0,0 +1,39 @@
+"""
+Preprocessor code
+Authors
+ * Amirali Ashraf 2022
+"""
+import numpy as np
+import torch
+import sklearn
+from sklearn.utils import shuffle
+
+
+class Preprocessor:
+    def __init__(
+            self,
+            path,
+    ):
+        self.path = path
+        self.X = None
+        self.y = None
+        with np.load(self.path) as data:
+            self.X = data['X'].astype(np.float32)
+            self.y = data['y'].astype(np.int64)
+
+    def as_numpy(self):
+        return self.X, self.y
+
+    def as_torch(self):
+        return torch.from_numpy(self.X), torch.from_numpy(self.y)
+
+    def as_shuffled_numpy(self):
+        return shuffle(self.X, self.y)
+
+    def as_shuffled_torch(self):
+        X, y = self.as_shuffled_numpy()
+        return torch.from_numpy(X), torch.from_numpy(y)
+
+
+
+
