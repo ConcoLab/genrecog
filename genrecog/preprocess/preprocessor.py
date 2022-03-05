5c390ab881bab482519eeea5016a5f15e0e222e6
amirali-ashraf <57413225+amiraliash@users.noreply.github.com>
2022-03-05 18:13:51 -0500

    clean ups on loading.

--- genrecog/preprocess/preprocessor.py
+++ genrecog/preprocess/preprocessor.py
@@ -15,20 +15,29 @@ class Preprocessor:
             path,
     ):
         self.path = path
-        self.X = None
-        self.y = None
-        with np.load(self.path) as data:
-            self.X = data['X'].astype(np.float32)
-            self.y = data['y'].astype(np.int64)
+        # self.X = None
+        # self.y = None
+        # with np.load(self.path) as data:
+        #     self.X = data['X'].astype(np.float32)
+        #     self.y = data['y'].astype(np.int64)
 
     def as_numpy(self):
-        return self.X, self.y
+        with np.load(self.path) as data:
+            X = data['X'].astype(np.float32)
+            y = data['y'].astype(np.int64)
+        return X, y
 
     def as_torch(self):
-        return torch.from_numpy(self.X), torch.from_numpy(self.y)
+        with np.load(self.path) as data:
+            X = data['X'].astype(np.float32)
+            y = data['y'].astype(np.int64)
+        return torch.from_numpy(X), torch.from_numpy(y)
 
     def as_shuffled_numpy(self):
-        return shuffle(self.X, self.y)
+        with np.load(self.path) as data:
+            X = data['X'].astype(np.float32)
+            y = data['y'].astype(np.int64)
+        return shuffle(X, y)
 
     def as_shuffled_torch(self):
         X, y = self.as_shuffled_numpy()
